bb7e37ec5e1f27f8c2a39212a159530f35383f82
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2024-08-19 08:34:37 -0700

    Update pseudobulk.py

--- pseudobulk.py
+++ pseudobulk.py
@@ -2,6 +2,7 @@ import scipy.io
 import numpy as np
 import sys
 import pandas as pd
+from scipy.sparse import coo_matrix, csr_matrix
 
 # Load the MatrixMarket file
 mm_file = sys.argv[1]  # Replace with your file path
@@ -15,8 +16,15 @@ pseudobulked_data_2d = np.array(pseudobulked_data).reshape(-1, 1)
 
 output_file = sys.argv[2]
 
+
 if output_file.endswith(".mtx"):
-  scipy.io.mmwrite(output_file, pseudobulked_data_2d)
+  subset_matrix = coo_matrix(pseudobulked_data_2d).T
+  with open(output_file, 'w') as f:
+    # Write the header for the MatrixMarket file
+    f.write(f"%%MatrixMarket matrix coordinate integer general\n")
+    f.write(f"{subset_matrix.shape[0]} {subset_matrix.shape[1]} {subset_matrix.nnz}\n")
+    for i, j, v in zip(subset_matrix.row, subset_matrix.col, subset_matrix.data):
+        f.write(f"{i + 1} {j + 1} {int(v)}\n")
   print(f'Pseudobulked data written to {output_file}')
   sys.exit(0)
 
